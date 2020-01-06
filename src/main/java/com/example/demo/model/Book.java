690014a65aacbfa0aa6eb2c3d3cdd5c5fcfc4598
buamod <buamod@gmail.com>
2020-01-05 19:23:12 -0800

    Adding Spring Data JPA Repositories

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,93 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String isbn;
+    private String publisher;
+    private  String title;
+
+    @ManyToMany
+    @JoinTable(name = "author_book", joinColumns = @JoinColumn(name = "book_id"), inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors = new HashSet<>();
+
+    public Book() {
+    }
+
+    public Book(String isbn, String publisher, String title) {
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.title = title;
+    }
+
+    public Book(String isbn, String publisher, String title, Set<Author> authors) {
+        this.isbn = isbn;
+        this.publisher = publisher;
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(String publisher) {
+        this.publisher = publisher;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return id.equals(book.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", publisher='" + publisher + '\'' +
+                ", title='" + title + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+}
